c6bd69468a08ea54e4602d2de61870c06ab18902
par333k <par333k@naver.com>
2021-01-29 15:20:56 +0900

    socket.io 적용

--- socket.js
+++ socket.js
@@ -1,3 +1,30 @@
+const SocketIO = require('socket.io');
+
+module.exports = (server) => {
+    const io = SocketIO(server, { path: '/socket.io' });
+
+    io.on('connection', (socket) => { // 웹소켓 연결 시
+        const req = socket.request;
+        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        console.log('새로운 클라이언트 접속!', ip, socket.id, req.ip);
+        socket.on('disconnect', () => { // 연결 종료 시
+            console.log('클라이언트 접속 해제', ip, socket.id);
+            clearInterval(socket.interval);
+        });
+        socket.on('error', (error) => { // 에러 시
+            console.error(error);
+        });
+        socket.on('reply', (data) => { // 클라이언트로부터 메세지
+            console.log(data);
+        });
+        socket.interval = setInterval(() => { // 3초마다 클라이언트로 메세지 전송
+            socket.emit('news', 'Hello Socket.IO'); // 첫 번째 인수는 이벤트 이름, 두 번째 인수는 데이터
+        }, 3000);
+    });
+};
+
+/*
+ws모듈 활용시 예제
 const WebSocket = require('ws');
 
 module.exports = (server) => {
@@ -23,4 +50,4 @@ module.exports = (server) => {
             }
         }, 3000);
     });
-};
+};*/
